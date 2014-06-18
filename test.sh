5df8b42090bf93c2f1d960d8f68280d82bdf4846
rbernand <rbernand@gmail.com>
2014-06-18 19:59:27 +0200

    clean dir

--- test.sh
+++
@@ -1,9 +0,0 @@
-# gunzip -c libpng-1.6.12.tar.gz | tar -xopf -
-# tar -zxvf libpng-1.6.12.tar.gz
-# tar -zxvf zlib-1.2.8.tar.gz
- cd libpng-1.6.12
- ./configure --prefix="/nfs/zfs-student-4/users/2013/tmielcza/Sujets/UNIX II/Zappy/ZGFX"
-# cp scripts/makefile.darwin makefile
-make
-make install
-make clean

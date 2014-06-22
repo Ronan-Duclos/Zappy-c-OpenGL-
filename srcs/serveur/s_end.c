8c698fcedb33fff3e0aa3b01aeb4cfb960bf879e
caupetit <caupetit@student.42.fr>
2014-06-22 18:37:23 +0200

    push before merge Rework incantation serveur

--- srcs/serveur/s_end.c
+++ srcs/serveur/s_end.c
@@ -6,7 +6,7 @@
 /*   By: rduclos <rduclos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/21 20:15:55 by rduclos           #+#    #+#             */
-/*   Updated: 2014/06/22 17:58:59 by rduclos          ###   ########.fr       */
+/*   Updated: 2014/06/22 18:16:14 by caupetit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,21 @@ void	verify_win(t_env *e, int cs)
 
 	i = -1;
 	if (e->msg_end == 1)
-		e->end = 1;
-	else
 	{
-		while (++i < e->nb_team)
-			if (ft_strcmp(e->team[i].name, e->users[cs]->player.team) == 0)
-				if (e->users[cs]->player.lvl == 8)
-				{
-					e->team[i].win++;
-					if (e->team[i].win == 6)
-					{
-						e->msg_end = 1;
-					}
-				}
+		e->end = 1;
+		return ;
+	}
+	while (++i < e->nb_team)
+	{
+		if (ft_strcmp(e->team[i].name, e->users[cs]->player.team) == 0 &&
+			e->users[cs]->player.lvl == 8)
+		{
+			e->team[i].win++;
+			if (e->team[i].win == 6)
+			{
+				e->msg_end = 1;
+				// add cmd de fin de jeux serveur.
+			}
+		}
 	}
 }

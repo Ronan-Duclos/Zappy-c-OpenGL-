7a6bc770b3e4c15d69d4920bb4d252da607a9412
Driss MANSOURI <dmansour@e1r11p16.42.fr>
2014-06-27 04:40:29 +0200

    display_words fonctionnel

--- srcs/gfx/g_resources.c
+++ srcs/gfx/g_resources.c
@@ -6,7 +6,7 @@
 /*   By: caupetit <caupetit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/06/13 22:20:29 by caupetit          #+#    #+#             */
-/*   Updated: 2014/06/26 17:10:24 by tmielcza         ###   ########.fr       */
+/*   Updated: 2014/06/27 03:06:02 by dmansour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,33 @@ static void	mdx_load(char *name, enum e_models mod, int geos)
 	ft_vbo_from_mdx(geosets->content, g_env->vbos[mod], g_env->vbosizes[mod]);
 }
 
+void		resources_load2(t_env *env)
+{
+	char	str[19] ="data/Police/am.png\0";
+	char	str2[18] ="data/Police/A.png\0";
+	int		i;
+
+	i = _ta;
+	while (str[12] <= 'z')
+	{
+		env->textures[i++] = texture_from_png(str);
+		str[12] += 1;
+	}
+	i = _tA;
+	while (str2[12] <= 'Z')
+	{
+		env->textures[i++] = texture_from_png(str2);
+		str2[12] += 1;
+	}
+	i = _t0;
+	str2[12] = '0';
+	while (str2[12] <= '9')
+	{
+		env->textures[i++] = texture_from_png(str2);
+		str2[12] += 1;
+	}
+}
+
 void		resources_load(t_env *env)
 {
 	env->maptex = texture_from_png("data/Grass.png");
@@ -38,7 +65,6 @@ void		resources_load(t_env *env)
 	env->zepptex = texture_from_png("data/GoblinZeppelin.png");
 	env->textures[_tex_egg] = texture_from_png("data/ThunderLizardEgg.png");
 	env->textures[_tex_totem] = texture_from_png("data/Wards1.png");
-	env->textures[_ta] = texture_from_png("data/Police/am.png");
 	mdx_load("data/CrystalShard.mdx", _mod_stone, 0);
 	mdx_load("data/Shimmerweed.mdx", _mod_plant, 0);
 	mdx_load("data/GoblinZeppelin.mdx", _mod_owl1, 0);
@@ -47,4 +73,5 @@ void		resources_load(t_env *env)
 	mdx_load("data/ThunderLizardEgg.mdx", _mod_egg, 0);
 	mdx_load("data/StasisTotem.mdx", _mod_totem, 3);
 	mdx_load("data/TalkToMe.mdx", _mod_talk, 0);
+	resources_load2(env);
 }
